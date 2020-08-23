fd147e2c4428b7f03571f72b6254a49b8cd6f051
H3x0R <59937884+OpenGamerTips@users.noreply.github.com>
2020-08-23 09:24:11 -0500

    Create mlg.lua

---
+++ tests/luacvm/mlg.lua
@@ -0,0 +1,131 @@
+getglobal game
+getfield -1 GetService
+pushvalue -2
+pushstring Players
+pcall 2 1 0
+getfield -1 LocalPlayer
+getfield -1 Character
+getfield -1 Head
+pushnumber 1
+setfield -2 Transparency
+emptystack
+getglobal game
+getfield -1 GetService
+pushvalue -2
+pushstring Players
+pcall 2 1 0
+getfield -1 LocalPlayer
+getfield -1 Character
+getfield -1 Head
+getfield -1 face
+pushnil
+setfield -2 Parent
+getglobal Instance
+getfield -1 new
+pushstring ParticleEmitter
+pushvalue -4
+pcall 2 1 0
+pushstring rbxassetid://158802305
+setfield -2 Texture
+pushnumber 100
+setfield -2 VelocitySpread
+emptystack
+getglobal game
+getfield -1 GetService
+pushvalue -2
+pushstring Players
+pcall 2 1 0
+getfield -1 LocalPlayer
+getfield -1 Character
+getfield -1 Head
+getglobal Instance
+getfield -1 new
+pushstring ParticleEmitter
+pushvalue -4
+pcall 2 1 0
+pushstring rbxassetid://176067516
+setfield -2 Texture
+pushnumber 100
+setfield -2 VelocitySpread
+emptystack
+getglobal game
+getfield -1 GetService
+pushvalue -2
+pushstring Players
+pcall 2 1 0
+getfield -1 LocalPlayer
+getfield -1 Character
+getfield -1 Head
+getglobal Instance
+getfield -1 new
+pushstring ParticleEmitter
+pushvalue -4
+pcall 2 1 0
+pushstring rbxassetid://217185480
+setfield -2 Texture
+pushnumber 100
+setfield -2 VelocitySpread
+emptystack
+getglobal game
+getfield -1 GetService
+pushvalue -2
+pushstring Players
+pcall 2 1 0
+getfield -1 LocalPlayer
+getfield -1 Character
+getfield -1 Head
+getglobal Instance
+getfield -1 new
+pushstring Sparkles
+pushvalue -4
+pcall 2 1 0
+emptystack
+getglobal workspace
+getglobal Instance
+getfield -1 new
+pushstring Sound
+pushvalue -4
+pcall 2 1 0
+pushstring rbxassetid://193528674
+setfield -2 SoundId
+pushboolean true
+setfield -2 Playing
+pushboolean true
+setfield -2 Looped
+emptystack
+getglobal game
+getfield -1 Lighting
+getglobal Instance
+getfield -1 new
+pushstring Sky
+pushvalue -4
+pcall 2 1 0
+pushstring illuminati
+setfield -2 Name
+pushstring rbxassetid://259226828
+setfield -2 SkyboxBk
+pushstring rbxassetid://259226828
+setfield -2 SkyboxDn
+pushstring rbxassetid://259226828
+setfield -2 SkyboxFt
+pushstring rbxassetid://259226828
+setfield -2 SkyboxLf
+pushstring rbxassetid://259226828
+setfield -2 SkyboxRt
+pushstring rbxassetid://259226828
+setfield -2 SkyboxUp
+clearstack
+getglobal game
+getfield -1 GetService
+pushvalue -2
+pushstring Players
+pcall 2 1 0
+getfield -1 LocalPlayer
+getfield -1 Character
+getfield -1 Shirt
+pushstring rbxassetid://329497491
+setfield -2 ShirtTemplate
+getfield -2 Pants
+pushstring rbxassetid://181887554
+setfield -2 PantsTemplate
+clearstack
